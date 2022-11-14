1ca07c070f4c5942d17d2ea538da03481906b475
Francis Ceril Challoy <francisceril.challoy@gmail.com>
2022-11-14 16:52:55 +0300

    cleaning

--- signpdf.py
+++ signpdf.py
@@ -1,24 +1,15 @@
 #!/usr/bin/env python
 
 import os
-import time
 import argparse
 import tempfile
-import PyPDF2
-import datetime
+from PyPDF2 import PdfFileReader, PdfFileWriter
 from tkinter import *
 from tkinter import filedialog
 from reportlab.pdfgen import canvas
 
 parser = argparse.ArgumentParser("Add signatures to PDF files")
-parser.add_argument("pdf", help="The pdf file to annotate")
-# parser.add_argument("signature", help="The signature file (png, jpg)")
-# parser.add_argument("--date", action='store_true')
-# parser.add_argument("--output", nargs='?',
-#         help="Output file. Defaults to input filename plus '_signed'")
-# parser.add_argument("--coords", nargs='?', default='1x200x0x170x100',
-#         help="Coordinates to place signature. Format: PAGExXxYxWIDTHxHEIGHT.  1x200x300x125x40 means page 1, 200 units horizontally from the bottom left, 300 units vertically from the bottom left, 125 units wide, 40 units tall. Pages count starts at 1 (1-based indexing).  Units are pdf-standard units (1/72 inch).")
-
+parser.add_argument("pdf", help="The pdf file to sign")
 
 def _get_tmp_filename(suffix=".pdf"):
     with tempfile.NamedTemporaryFile(suffix=".pdf") as fh:
@@ -33,8 +24,8 @@ def sign_pdf(pdfFile):
     sig_tmp_fh = None
     signature = "sign.png"
 
-    pdf = PyPDF2.PdfFileReader(pdf_fh)
-    writer = PyPDF2.PdfFileWriter()
+    pdf = PdfFileReader(pdf_fh)
+    writer = PdfFileWriter()
     sig_tmp_filename = None
 
     for i in range(0, pdf.getNumPages()):
@@ -51,7 +42,7 @@ def sign_pdf(pdfFile):
 
             # Merge PDF in to original page
             sig_tmp_fh = open(sig_tmp_filename, 'rb')
-            sig_tmp_pdf = PyPDF2.PdfFileReader(sig_tmp_fh)
+            sig_tmp_pdf = PdfFileReader(sig_tmp_fh)
             sig_page = sig_tmp_pdf.getPage(0)
             sig_page.mediaBox = page.mediaBox
             page.mergePage(sig_page)
